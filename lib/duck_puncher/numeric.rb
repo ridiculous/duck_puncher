a93ef68b8d1647f1c71356a3004e7de89a0cb924
Ryan Buckley <arebuckley@gmail.com>
2014-03-24 21:00:44 -0700

    add Numeric#to_duration

--- lib/duck_puncher/numeric.rb
+++ lib/duck_puncher/numeric.rb
@@ -3,6 +3,25 @@ module DuckPuncher
     def to_currency
       '%.2f' % self.round(2)
     end
+
+    def to_duration(seconds=false)
+      secs = self.to_i
+      mins = secs / 60
+      hours = mins / 60
+      buffer = ''
+      if hours > 0
+        num_mins = mins % 60
+        buffer << "#{hours} h"
+        buffer << " #{num_mins} min" unless num_mins.zero?
+      elsif mins > 0
+        num_secs = secs % 60
+        buffer << "#{mins} min"
+        buffer << " #{num_secs} s" if seconds && num_secs.nonzero?
+      elsif seconds && secs >= 0
+        buffer << "#{secs} s"
+      end
+      buffer
+    end
   end
 end
 
--- test/duck_puncher/numeric_test.rb
+++ test/duck_puncher/numeric_test.rb
@@ -8,4 +8,19 @@ class NumericTest < MiniTest::Unit::TestCase
     assert_equal '25.20', 25.2.to_currency
     assert_equal '25.25', 25.245.to_currency
   end
+
+  def test_to_duration
+    assert_equal '', 10.to_duration
+    assert_equal '1 min', 100.to_duration
+    assert_equal '16 min', 1_000.to_duration
+    assert_equal '2 h 46 min', 10_000.to_duration
+    assert_equal '27 h 46 min', 100_000.to_duration
+  end
+
+  def test_to_duration_with_seconds
+    assert_equal '10 s', 10.to_duration(true)
+    assert_equal '1 min 40 s', 100.to_duration(true)
+    assert_equal '16 min 40 s', 1_000.to_duration(true)
+    assert_equal '2 h 46 min', 10_000.to_duration(true)
+  end
 end
