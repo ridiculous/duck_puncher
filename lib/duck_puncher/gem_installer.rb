2bee6245a6551b7b6190d686634f8a1ca301ae1d
Ryan Buckley <arebuckley@gmail.com>
2015-12-14 18:35:50 -0800

    replace load path crap with bundler.bundle_path

--- lib/duck_puncher/gem_installer.rb
+++ lib/duck_puncher/gem_installer.rb
@@ -22,10 +22,10 @@ class DuckPuncher::GemInstaller
   # @param [String] version of the gem to install (e.g. '1.2.3')
   def perform(*args)
     require 'rubygems/dependency_installer'
-    installer = Gem::DependencyInstaller.new(install_dir: load_path.to_s, bin_dir: RbConfig::CONFIG['bindir'])
+    installer = Gem::DependencyInstaller.new(install_dir: Bundler.bundle_path.to_s, bin_dir: RbConfig::CONFIG['bindir'])
     installer.install *args.reject(&:empty?)
     installer.installed_gems.each do |gem|
-      full_load_path = load_path.join('gems', "#{gem.name}-#{gem.version}", "lib")
+      full_load_path = Bundler.bundle_path.join('gems', "#{gem.name}-#{gem.version}", "lib")
       next if $LOAD_PATH.include?(full_load_path.to_s)
       $LOAD_PATH.unshift full_load_path.to_s
       DuckPuncher::JSONStorage.write 'load_paths.json', args.first, full_load_path
@@ -33,23 +33,5 @@ class DuckPuncher::GemInstaller
     installer.installed_gems.any?
   rescue => e
     puts "Failed to install #{args.first}. #{e.inspect}"
-  end
-
-  def load_path
-    @load_path ||= begin
-      spec = Bundler.definition.specs.first
-      spec_pattern = %r[(gems/#{Regexp.escape(spec.name)}-[#{Regexp.escape(spec.version.to_s.tr(' ', '-'))}]+/.*)]
-      current_load_path = find_load_path(spec_pattern)
-      if current_load_path
-        Pathname.new current_load_path.sub(spec_pattern, '')
-      else
-        fail "Coudn't find example load path to use for the download!"
-      end
-    end
-  end
-
-  def find_load_path(spec_pattern)
-    load_paths = $LOAD_PATH + Bundler::RubygemsIntegration.new.loaded_gem_paths
-    load_paths.find { |x| x =~ spec_pattern }
   end
 end
