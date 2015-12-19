ef1045ce435fe83fc57b536284d899f0b554a659
Ryan Buckley <arebuckley@gmail.com>
2015-12-18 18:47:46 -0800

    Add method definition helper because Method#source was defined by other gem

--- lib/duck_puncher/method.rb
+++ lib/duck_puncher/method.rb
@@ -1,7 +1,41 @@
 module DuckPuncher
   module Method
     def to_instruct
-      RubyVM::InstructionSequence.new(source).disasm
+      RubyVM::InstructionSequence.new(Definition.new(self).lines.join).disasm
+    end
+
+    class Definition
+      def initialize(method_handle)
+        @file_path, @line_num = *method_handle.source_location
+        @line_num = @line_num.to_i
+      end
+
+      # @description finds the method's source code using the indent size of the file. This means we are
+      # restricted when it comes to parsing crappy formatted ruby files
+      def lines
+        return @lines if defined? @lines
+        fail "Couldn't find the source location for that method" unless @file_path
+        @lines = []
+        File.open(@file_path) do |f|
+          found = false
+          i = 0
+          while line = f.gets and i += 1 and !found
+            next if i < @line_num
+            @lines << line
+            if @indent_size
+              found = @indent_size == find_indent_size(line)
+            else
+              @indent_size = find_indent_size(line)
+              found = line.end_with?("end\n")
+            end
+          end
+        end
+        @lines
+      end
+
+      def find_indent_size(line)
+        line[/(\s*)/].size
+      end
     end
   end
 end
